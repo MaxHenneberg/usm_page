76c5e6ca4aa2ffec330a21d0f2ccdd4a55f1772d
Max Henneberg <henneberg.maximilian@googlemail.com>
2021-03-14 13:58:53 +0100

    Fixed Navbar for Production

--- src/components/header/HeaderComponent.js
+++ src/components/header/HeaderComponent.js
@@ -9,29 +9,42 @@ import FormCheck from "react-bootstrap/FormCheck";
 import "flag-icon-css/css/flag-icon.css"
 import "./HeaderComponent.css"
 import {LanguageContext} from "../../App";
+import Dropdown from "react-bootstrap/Dropdown";
+import Nav from "react-bootstrap/Nav";
 
 function HeaderComponent(props) {
   const languageContext = useContext(LanguageContext);
+  const [expanded, setExpanded] = useState(false);
   return (
       <div>
-              <Navbar bg={"light"} expand="md" fixed="top">
+              <Navbar bg={"light"} expand="md" fixed="top" className="justify-content-end" expanded={expanded} onToggle={() => setExpanded(!expanded)}>
                 <NavbarToggle aria-controls="basic-navbar-nav"/>
-                <NavbarCollapse id="basic-navbar-nav" className="justify-content-end">
-                  <NavLink href='#/'>{languageContext.content.header.home}</NavLink>
-                  <NavLink href="/#faq">FAQ</NavLink>
-                  <NavLink href="#/help">{languageContext.content.header.helped}</NavLink>
-                  <NavDropdown title={languageContext.content.header.support} id="basic-nav-dropdown">
-                    <NavDropdown.Item href='#/volunteer'>Become a Volunteer!</NavDropdown.Item>
-                    <NavDropdown.Item href="#action/3.2">Donate!</NavDropdown.Item>
-                  </NavDropdown>
-                  <NavLink href='#/team'>{languageContext.content.header.team}</NavLink>
-                  <Form inline>
-                    <div className="horizontalCenter">
-                      <i className="flag-icon-my flag-icon float-left vertical-center"/>
-                      <FormCheck type="switch" id="custom-switch" className="float-left" onChange={() =>{props.updateLanguage()}} checked={languageContext.language !== 'my'}/>
-                      <i className="flag-icon-gb-eng flag-icon float-left vertical-center"/>
-                    </div>
-                  </Form>
+                <NavbarCollapse id="basic-navbar-nav">
+                  <Nav className="mr-auto justify-content-end" onSelect={() => setExpanded(!expanded)}>
+                    <NavLink href='/#'>{languageContext.content.header.home}</NavLink>
+                    <NavLink href="/#faq">FAQ</NavLink>
+                    <NavLink href="#/help">{languageContext.content.header.helped}</NavLink>
+                    <NavDropdown title={languageContext.content.header.support} id="basic-nav-dropdown">
+                      <NavDropdown.Item href='#/volunteer'>Become a Volunteer!</NavDropdown.Item>
+                      <NavDropdown.Item href="#action/3.2">Donate!</NavDropdown.Item>
+                    </NavDropdown>
+                    <NavLink href='#/team'>{languageContext.content.header.team}</NavLink>
+                  </Nav>
+                  <div className="horizontalCenter-sm">
+                    <Form inline>
+                      <Dropdown onSelect={() =>{props.updateLanguage();setExpanded(!expanded);}}>
+                        <Dropdown.Toggle id="dropdown-basic" className="p-1">
+                          {languageContext.language=='my' && <span className="flag-icon-my flag-icon flag mr-1"/>}
+                          {languageContext.language=='en' && <span className="flag-icon-gb-eng flag-icon flag mr-1"/>}
+                        </Dropdown.Toggle>
+
+                        <Dropdown.Menu>
+                          {languageContext.language=='my' && <Dropdown.Item><div className="flag-icon-gb-eng flag-icon flag"/></Dropdown.Item>}
+                          {languageContext.language=='en' && <Dropdown.Item><div className="flag-icon-my flag-icon flag"/></Dropdown.Item>}
+                        </Dropdown.Menu>
+                      </Dropdown>
+                    </Form>
+                  </div>
                 </NavbarCollapse>
               </Navbar>
       </div>
