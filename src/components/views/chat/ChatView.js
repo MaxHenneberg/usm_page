be422dae09ce4850c1951fb60e5fd2b7f20410a7
Malfurius <henneberg.maximilian@googlemail.com>
2021-04-02 12:22:45 +0200

    added FeedbackForm to chat

--- src/components/views/chat/ChatView.js
+++ src/components/views/chat/ChatView.js
@@ -7,37 +7,46 @@ import chatJson from "../../../contrib/content/chat"
 import {LanguageContext} from "../../../App";
 
 function ChatView(props) {
-  const languageContext = useContext(LanguageContext);
-  const [show, setShow] = useState(true);
-  let chat = chatJson.en;
-  if (languageContext.language === 'my') {
-    chat = chatJson.my;
-  }
+    const languageContext = useContext(LanguageContext);
+    const [show, setShow] = useState(true);
+    let chat = chatJson.en;
+    if (languageContext.language === 'my') {
+        chat = chatJson.my;
+    }
 
-  return (
-      <div>
-        <Modal backdrop="static" show={show} onHide={() => setShow(false)}>
-          <Modal.Header>
-            <Modal.Title>{chat.modal.title}</Modal.Title>
-          </Modal.Header>
+    return (
+        <div>
+            <Modal backdrop="static" show={show} onHide={() => setShow(false)}>
+                <Modal.Header>
+                    <Modal.Title>{chat.modal.title}</Modal.Title>
+                </Modal.Header>
 
-          <Modal.Body>
-            {chat.modal.rules.map((rule) => {
-              return(
-                  <p>{rule}</p>
-              )
-            })}
-            <p>{chat.modal.text}</p>
-          </Modal.Body>
-          <Modal.Footer>
-            <Button variant="primary" onClick={() => setShow(false)}>{chat.modal.footer.primaryButton}</Button>
-          </Modal.Footer>
-        </Modal>
-        <div className="mt-">
-          <iframe src='https://deadsimplechat.com/OZCgJ4twA' width='100%' height='600px'></iframe>
+                <Modal.Body>
+                    {chat.modal.rules.map((rule) => {
+                        return (
+                            <p>{rule}</p>
+                        )
+                    })}
+                    <p>{chat.modal.text}</p>
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="primary" onClick={() => setShow(false)}>{chat.modal.footer.primaryButton}</Button>
+                </Modal.Footer>
+            </Modal>
+            <div className="mt-">
+                <iframe src='https://deadsimplechat.com/OZCgJ4twA' width='100%' height='600px'></iframe>
+            </div>
+            <div className="chatFeedback">
+                <div>
+                    Used our Service ? Have Feedback ? Please let us know:
+                </div>
+                <a href='https://docs.google.com/forms/d/e/1FAIpQLSdyPtOfIKj6rVI0eUY9l4j5_ZYR6APHTrT5uLpD7e6zHLYQIQ/viewform?usp=sf_link'
+                   target="_blank">
+                    <Button variant="primary">Feedback Form</Button>
+                </a>
+            </div>
         </div>
-      </div>
-  )
+    )
 }
 
 export default ChatView;
